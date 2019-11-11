c88ab8559d08c0c8b12c3930d16759e8cd270326
Corbin Davenport <me@corbin.io>
2019-11-11 01:56:56 -0500

    Improve plugin spoofing

--- js/noplugin-compat.js
+++ js/noplugin-compat.js
@@ -24,30 +24,43 @@ if (!navigator.plugins.namedItem('Shockwave Flash') && document.cookie.includes(
                 writable: false
             })
         }
+
         // Define list of plugins and supported MIME types
+
         Object.defineProperties(modifiedNavigator, {
             plugins: {
-                value: [{
-                    name: 'Shockwave Flash',
-                    description: 'Simulated Flash Player activated by NoPlugin Compatibility Mode',
-                    version: '100',
-                    filename: 'NoPlugin'
-                }],
+                value: {
+                    'Shockwave Flash': {
+                        name: 'Shockwave Flash',
+                        description: 'Shockwave Flash 50.0 r0 (NoPlugin)',
+                        version: '50.0',
+                        filename: 'NoPlugin'
+                    }
+                },
                 configurable: false,
                 enumerable: true,
                 writable: false
-                
-            },
+            }
+        })
+
+        // Define list of supported MIME types and their linked plugins
+
+        Object.defineProperties(modifiedNavigator, {
             mimeTypes: {
-                value: [{
-                    type: 'application/x-shockwave-flash',
-                    suffixes: 'swf',
-                    description: 'Shockwave Flash'
-                }, {
-                    type: 'application/futuresplash',
-                    suffixes: 'spl',
-                    description: 'Shockwave Flash'
-                }],
+                value: {
+                    'application/x-shockwave-flash': {
+                        type: 'application/x-shockwave-flash',
+                        suffixes: 'swf',
+                        description: 'Shockwave Flash',
+                        enabledPlugin: navigator.plugins['Shockwave Flash']
+                    },
+                    'application/futuresplash': {
+                        type: 'application/futuresplash',
+                        suffixes: 'spl',
+                        description: 'Shockwave Flash',
+                        enabledPlugin: navigator.plugins['Shockwave Flash']
+                    }
+                },
                 configurable: false,
                 enumerable: true,
                 writable: false
@@ -60,12 +73,13 @@ if (!navigator.plugins.namedItem('Shockwave Flash') && document.cookie.includes(
                 writable: false
             },
         })
-        // Define functions for plugin access
+
+        // Define functions for navigator.plugins
         Object.defineProperties(modifiedNavigator.plugins, {
             namedItem: {
                 // The navigator.plugins.namedItem() function is used to obtain information about the plugin specified in the argument
-                value: function(plugin) {
-                    var info = navigator.plugins.find(a => a['name'] === plugin)
+                value: function (plugin) {
+                    var info = navigator.plugins.hasOwnProperty(plugin)
                     if (info) {
                         // If there is a valid JSON object, return it
                         return info
@@ -73,8 +87,8 @@ if (!navigator.plugins.namedItem('Shockwave Flash') && document.cookie.includes(
                         // If the desired plugin is not implemented by NoPlugin, try to generate something the page will accept
                         var array = {
                             name: plugin,
-                            description: 'Simulated ' + plugin + ' activated by NoPlugin Compatibility Mode',
-                            version: '100',
+                            description: 'Simulated ' + plugin + ' 50.0 (NoPlugin)',
+                            version: '50.0',
                             filename: 'NoPlugin'
                         }
                         return array
@@ -85,14 +99,35 @@ if (!navigator.plugins.namedItem('Shockwave Flash') && document.cookie.includes(
                 writable: false
             }, refresh: {
                 // The navigator.plugins.refresh() function is used to refresh the list of currently-installed applications
-                value: function() {
+                value: function () {
                     return
+                },
+                configurable: false,
+                enumerable: true,
+                writable: false
+            }, length: {
+                // The navigator.plugins.length function returns the number of plugins installed
+                value: Object.keys(navigator.plugins).length,
+                configurable: false,
+                enumerable: true,
+                writable: false
+            }
+        })
+
+        // Define functions for navigator.mimeTypes
+
+        Object.defineProperties(modifiedNavigator.mimeTypes, {
+            length: {
+                // The navigator.mimeTypes.length function returns the number of mimeTypes supported
+                value: function () {
+                    return Object.keys(navigator.mimeTypes).length
                 },
                 configurable: false,
                 enumerable: true,
                 writable: false
             }
         })
+
     } + ')()'
 
     var s = document.createElement('script')
