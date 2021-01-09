bac8a19310f1074ec6bd363f6c3d3f492d21410f
Corbin Davenport <me@corbin.io>
2021-01-08 22:19:26 -0500

    Update version to v7.0.1, remove email bug reporting

--- js/bugreport.js
+++ js/bugreport.js
@@ -9,7 +9,3 @@ function getUrlVars() {
 document.querySelector('.github-button').addEventListener('click', function () {
     window.open('https://github.com/corbindavenport/noplugin/issues/new', '_blank')
 })
-
-document.querySelector('.email-button').addEventListener('click', function () {
-    window.location.href = 'mailto:noplugin@fire.fundersclub.com?subject=NoPlugin%20not%20working%20on%20' + getUrlVars()['url'] + '&body=Please%20describe%20the%20issue%3A%20'
-})
